b5a3f31dfaf746a4ed7393f4cb555fbe705378c6
Nícolas Goulart <nicolas.goulartsouza123@gmail.com>
2022-01-28 11:10:04 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const info ={
+    nome: "Nicolas",
+    idade: "18 anos",
+    humano: true
+}
+
+info.estado = "MG"
+delete info.estado
+console.log(info)
+
+let cadastro = {
+    nome: "Nicolas",
+    idade: 18,
+    telefone: "(32)98477-0122",
+    amigos: ["Vanessa", "Gabriel", "Samira", "Miqueias"]
+}
+console.log(cadastro.amigos)
